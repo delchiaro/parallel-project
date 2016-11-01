558117b82bc39b62ba34c1fe176f6e11a923c0c6
Nagash <nagash91@gmail.com>
2016-11-01 17:53:56 +0100

    ForceInline vs noInline test prepared

---
+++ bin/inline_vs_no_inline.sh
@@ -0,0 +1,9 @@
+cd debug-O0
+./ObjectOriented-forceInline          imgBig.jpg  -v -i 40  -t 4 -t 8 -t 16 -t 32  -ctn forceInline.csv
+./ObjectOriented-forceInlineDisabled  imgBig.jpg  -i 40  -t 4 -t 8 -t 16 -t 32  -ctn forceInlineDisabled.csv
+./ObjectOriented-inlineDisabled       imgBig.jpg  -i 40  -t 4 -t 8 -t 16 -t 32  -ctn inlineDisabled.csv
+
+cd ../debug-O1
+./ObjectOriented-forceInline          imgBig.jpg  -i 40  -t 4 -t 8 -t 16 -t 32  -ctn forceInline.csv
+./ObjectOriented-forceInlineDisabled  imgBig.jpg  -i 40  -t 4 -t 8 -t 16 -t 32  -ctn forceInlineDisabled.csv
+./ObjectOriented-inlineDisabled       imgBig.jpg  -i 40  -t 4 -t 8 -t 16 -t 32  -ctn inlineDisabled.csv
